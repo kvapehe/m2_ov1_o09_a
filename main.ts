a0a012f390b698710aab5fd8b49e5e4ac2bff8e0
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-11-12 22:16:13 +0100

    Levert

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-
+input.onButtonPressed(Button.A, function () {
+    tall += 1
+})
+let tall = 0
+basic.showNumber(tall)
+basic.forever(function () {
+    if (tall >= 10) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showNumber(tall)
+    }
+})
